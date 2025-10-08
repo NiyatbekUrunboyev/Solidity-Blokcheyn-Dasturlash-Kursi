c1ddbeede03cd1a85dbd2fb271593a429414565b
Niyatbek Urunboyev <nurunboyev@bk.ru>
2025-10-08 15:17:54 +0500

    added 2-dars

---
+++ Yechimlar/2-dars yechim.sol
@@ -0,0 +1,8 @@
+//SPDX-Licesse-Identifier: MIT
+pragma solidity >=0.8.30>=0.9.0;
+contract DataTypes {
+    string public name = "Asad";
+    bool public isYoung = true;
+    address public hisAddress = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+    bytes32 public hash = 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef;
+}   
